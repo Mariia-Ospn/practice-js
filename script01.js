55c85250eb8e397fdd1b378e2d02fbad66cee9b4
Mariia-Ospn <126498309+Mariia-Ospn@users.noreply.github.com>
2023-06-08 19:18:08 +0200

    подготовка к работе

---
+++ script01.js
@@ -0,0 +1,14 @@
+// Change code below this line
+function filterArray(numbers, value) {
+    const filteredNumbers = [];
+  
+    numbers.forEach(function (number) {
+      if (number > value) {
+        filteredNumbers.push(number);
+      }
+    });
+  
+    // Change code above this line
+    return filteredNumbers;
+  }
+  
